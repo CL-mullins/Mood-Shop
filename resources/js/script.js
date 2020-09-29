ccc051e0fad2467db937f2713d9c866b32f644eb
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-09-29 16:14:28 -0700

    cart fully functional

--- resources/js/script.js
+++ resources/js/script.js
@@ -59,12 +59,44 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
   }))
 
 const cart = []
+// -----------------------------------------------------------
+// Handle change events on update input
+itemList.onchange = function(e) {
+    if (e.target && e.target.classList.contains('update')) {
+        const name = e.target.dataset.name
+        const qty = parseInt(e.target.value)
+        updateCart(name, qty)
+    }
+}
+
+
+// -----------------------------------------------------------
+// Handle clicks on list
+itemList.onclick = function(e) {
+    if (e.target && e.target.classList.contains('remove')) {
+        console.log()
+        const name = e.target.dataset.name // data-name = "?????"
+        removeItem(name)
+    }
+    else if (e.target && e.target.classList.contains('add-one')) {
+        const name = e.target.dataset.name
+        addItem(name)
+    }
+    else if (e.target && e.target.classList.contains('remove-one')) {
+        const name = e.target.dataset.name
+        removeItem(name, 1)
+
+    }
+}
+
+
 // --------------------------------------------------------
 // Add item
     function addItem(name, price) {
         for (let i = 0; i < cart.length; i += 1) {
             if (cart[i].name === name) {
                 cart[i].qty += 1
+                showItems()
                 return
             }
         }
@@ -86,7 +118,13 @@ const cart = []
             const qty = cart[i].qty
            // const { name, price, qty} = cart[i]
 
-            itemStr +=`<li> ${name} $${price} x ${qty} = ${qty * price}</li>`
+            itemStr +=`<li>
+             ${name} $${price} x ${qty} = ${qty * price}
+             <button class = "remove" data-name="${name}"> Remove </button>
+             <button class = "add-one" data-name="${name}"> + </button>
+             <button class = "remove-one" data-name="${name}"> - </button>
+             <input class="update" type="number" data-name="${name}">
+             </li>`
         }
         itemList.innerHTML = itemStr
 
@@ -116,7 +154,7 @@ const cart = []
     }
 // ------------------------------------------
 // Remove item
-function removeItem(name) {
+function removeItem(name, qty = 0) {
     for (let i = 0; i < cart.length; i += 1) {
         if (cart[i].name === name) {
             if (qty > 0) {
@@ -126,9 +164,23 @@ function removeItem(name) {
             if (cart[i].qty < 1 || qty == 0) {
                 cart.splice(i, 1)
             }
+            showItems()
             return
         }
     }
 }
 
+// --------------------------------------------
+function updateCart(name, qty) {
+    for (let i=0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty < 1) {
+                removeItem(name)
+            }
+            cart[i].qty = qty
+            showItems()
+            return
+        }
+    }
+}
 
